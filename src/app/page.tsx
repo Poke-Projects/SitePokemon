9b94a190abe7d186ab3c62c48933ac4d66729f4e
DiegoAndre Leffa <diegodocanto@gmail.com>
2024-01-30 09:46:49 -0300

    feat: homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,23 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import map from "../../public/map.svg";
+import teambuilder from "../../public/teambuilder.svg";
+import battlesimulator from "../../public/battlesimulator.svg";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-        <h1>Pokemon</h1>
+      <h1>Pokemon</h1>
+
+      <a href="">
+        <Image src={teambuilder} alt="" />
+      </a>
+      <a href="">
+        <Image src={map} alt="" />
+      </a>
+      <a href="">
+        <Image src={battlesimulator} alt="" />
+      </a>
     </main>
   );
 }
